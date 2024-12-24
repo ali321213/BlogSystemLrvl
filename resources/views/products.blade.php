6ecb6156fc82b2756bfc7d4a634c1933fc0ca1e8
Ali <ra38608341@gmail.com>
2024-12-24 21:09:11 +0500

    first commit

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -363,9 +363,7 @@
             });
         </script>
 
-        <script src="https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js"></script>
-        <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
+        <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>a
         <script src="{{ asset('assets/vendors/chart.js/Chart.min.js') }}"></script>
         <script src="{{ asset('assets/js/jquery.cookie.js" type="text/javascript') }}"></script>
         <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
